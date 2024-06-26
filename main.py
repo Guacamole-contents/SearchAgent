a8a1b6aec0d8c3bc670dd0b8b3a1dfa7b9d1c95f
Jeon-jisu <wltnjeon0119@gmail.com>
2024-06-27 00:18:52 +0900

    [Refactor] generate_prompt 파일 리팩토링으로 인한 함수이름 변경 업데이트

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import json
 
 from getmeta import makevideopair
-from generate_prompt import generate_prompt
+from generate_prompt import generate_violation_detection_prompt
 from save_result_to_db import save_result_to_db
 from request_to_llm import request_to_llm
 
@@ -13,9 +13,14 @@ from request_to_llm import request_to_llm
 def main():
     # 입력 인자값 설정.
     parser = argparse.ArgumentParser(
-        description='YouTube 영상의 코드를 이용하여 위반 영상을 찾아 DB에 저장합니다. ')
-    parser.add_argument('--code', metavar='c', type=str,
-                        help='YouTube의 영상코드. (Example: UP2RFQCszdk)')
+        description="YouTube 영상의 코드를 이용하여 위반 영상을 찾아 DB에 저장합니다. "
+    )
+    parser.add_argument(
+        "--code",
+        metavar="c",
+        type=str,
+        help="YouTube의 영상코드. (Example: UP2RFQCszdk)",
+    )
     args = parser.parse_args()
 
     video_code = args.code
@@ -29,8 +34,9 @@ def main():
             violate_meta = data["violate_meta"]
             violate_comment = data["violate_comment"]
 
-            prompt = generate_prompt(
-                origin_meta, origin_comment, violate_meta, violate_comment)
+            prompt = generate_violation_detection_prompt(
+                origin_meta, origin_comment, violate_meta, violate_comment
+            )
             print(prompt)
 
             result_llm = request_to_llm(prompt)
@@ -40,13 +46,19 @@ def main():
 
             print(">>> LLM Query result:", json_result)
             print(">>> Save result")
-            print({"origin_video_name": video_code, "copy_video_name": violate_meta["Original video code"],
-                   "is_copy": json_result["is_copy"], "reason": json_result["reason"]})
+            print(
+                {
+                    "origin_video_name": video_code,
+                    "copy_video_name": violate_meta["Original video code"],
+                    "is_copy": json_result["is_copy"],
+                    "reason": json_result["reason"],
+                }
+            )
 
             # save_result_to_db(
             #     [{"origin_video_name": video_code, "copy_video_name": violate_meta["Original video code"],
             #      "is_copy": json_result["is_copy"], "reason": json_result["reason"], "search_date": int(datetime.now().timestamp())}])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
